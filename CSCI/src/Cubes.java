174930a8a2163a75f5d1820e0ecf03bcabe3d83c
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-01-17 15:57:01 +0100

    c2

--- CSCI/src/Cubes.java
+++ CSCI/src/Cubes.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Cubes {
     public static void main(String[] args) {
-        int max=Integer.parseInt(args[0]);
+        Scanner sc=new Scanner(System.in);
+        int max=sc.nextInt();
         for (int i = 0; i <= max; i++) {
             int pth=(int)Math.round(Math.pow(i,1/3.0));
             if(i==pth*pth*pth){
