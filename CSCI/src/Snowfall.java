e4581710c56de46d58ea75899bab7426f3620c69
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-01-17 17:28:47 +0100

    s4

--- CSCI/src/Snowfall.java
+++ CSCI/src/Snowfall.java
@@ -4,23 +4,20 @@ public class Snowfall {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             int n=sc.nextInt();
-            int[] snow=new int[n];
+            int[] snow=new int[n+1];
             for (int i = 0; i < n; i++) {
-                snow[i]=sc.nextInt();
+                snow[i+1]=sc.nextInt();
             }
 
             //begin algorithm
-            int sum=snow[0]+snow[1]+snow[2];
-            int bigger_3d_interval=sum;
-            int current_3d_interval=sum;
+            int seuil=snow[n]/2;
             for (int i = 3; i < n; i++) {
-                sum+=snow[i];
-                current_3d_interval+=snow[i]-snow[i-3];
-                if(current_3d_interval>bigger_3d_interval)bigger_3d_interval=current_3d_interval;
+                if(snow[i]-snow[i-3]>seuil){
+                    System.out.println("YES");
+                    return;
+                }
             }
-            if(2*bigger_3d_interval>sum){
-                System.out.println("YES");
-            }else System.out.println("NO");
+            System.out.println("NO");
         }
 
 }
