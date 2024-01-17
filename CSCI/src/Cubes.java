7f30327c2e81b0f1ed1ed8a06ee732359c147746
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-01-17 15:44:14 +0100

    Cubes

---
+++ CSCI/src/Cubes.java
@@ -0,0 +1,14 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Cubes {
+    public static void main(String[] args) {
+        int max=Integer.parseInt(args[0]);
+        for (int i = 0; i <= max; i++) {
+            int pth=(int)Math.round(Math.pow(i,1/3.0));
+            if(i==pth*pth*pth){
+                System.out.println(i);
+            }
+        }
+    }
+}
