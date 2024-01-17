71f95d5f939c5b6273d9f872ae7c710fd076a01c
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-01-17 17:18:48 +0100

    s2

--- CSCI/src/snowfall.java
+++ CSCI/src/snowfall.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class snowfall {
+public class Snowfall {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
