7e195ecd2fffd7afb10e1260dcb31132053b1ffc
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-03-27 10:15:53 -0400

    Sub V2

--- CSCI/src/Sub.java
+++ CSCI/src/Sub.java
@@ -1,11 +1,30 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
 
 public class Sub {
-   public static void main(String[]args){
+    public static int findIndex(int[][][] array,int high,int channel, int begin) {
+        int low = 0;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid][channel][1] <= begin) {
+                index = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return index;
+    }
+
+   public static void main(String[]args) throws FileNotFoundException {
        Scanner sc = new Scanner(System.in);
+       sc = new Scanner(new FileReader(new File("res/input-1.txt")));
        int n = sc.nextInt();
 
        List<int[]>jobsA=new ArrayList<>();
@@ -14,9 +33,11 @@ public class Sub {
            int b=sc.nextInt();
            int e=sc.nextInt();
            int c=sc.nextInt();
+
            jobsA.add(new int[]{b,e,c});
 
        }
+      
        //sort the job by increasing finishing time
        Comparator<int[]>comp=new Comparator<int[]>() {
            @Override
@@ -41,28 +62,46 @@ public class Sub {
            }
            //if we switch from contractor
            //check if the the new contract begin after the end of the previous and if the new length is superior to the previous
-            if(array[i-1][1-contractor][1]<=begin && array[i-1][contractor][0]<array[i-1][1-contractor][0]+1){
-               array[i][contractor][0]=array[i-1][1-contractor][0]+1;
-               array[i][contractor][1]=end;
-           }
-           if(array[i-1][3-contractor][1]<=begin && array[i][contractor][0]<array[i-1][3-contractor][0]+1){
-               array[i][contractor][0]=array[i-1][3-contractor][0]+1;
-               array[i][contractor][1]=end;
-           }
+            int index=i-1;
+           index=findIndex(array,i-1,1-contractor,begin);
+           //while (index>0 && A[index]>begin)index--;
+            if(array[index][1-contractor][1]<=begin && array[i][contractor][0]<array[index][1-contractor][0]+1){
 
+                    array[i][contractor][0] = array[index][1 - contractor][0] + 1;
+                    array[i][contractor][1] = end;
+
+           }
+           index=i-1;
+           //while (index>0 && array[index][3-contractor][1]>begin)index--;
+           index=findIndex(array,i-1,3-contractor,begin);
+           if(array[index][3-contractor][1]<=begin && array[i][contractor][0]<array[index][3-contractor][0]+1){
+
+                   array[i][contractor][0] = array[index][3 - contractor][0] + 1;
+                   array[i][contractor][1] = end;
+
+           }
+           index=i-1;
+           //while (index>0 && array[index][contractor][1]>begin)index--;
+           index=findIndex(array,i-1,contractor,begin);
            //if we stay with the same contractor for two consecutive contracts.
-           if(array[i-1][contractor][1]<=begin && array[i-1][2+contractor][0]<array[i-1][contractor][0]+1){
-               array[i][2+contractor][0]=array[i-1][contractor][0]+1;
-               array[i][2+contractor][1]=end;
+           if(array[index][contractor][1]<=begin && array[i][2+contractor][0]<array[index][contractor][0]+1 && array[index][contractor][0]>0){
+
+                   array[i][2 + contractor][0] = array[index][contractor][0] + 1;
+                   array[i][2 + contractor][1] = end;
+
            }
 
        }
+
         //find the number of contract
        int m=array[n][0][0];
+
        for (int i = 1; i < 4; i++) {
           m= Math.max(m,array[n][i][0]);
        }
+
        System.out.println(m);
+       //System.out.println(System.currentTimeMillis()-time);
    }
 }
 
