b8f7f3b4429fdbedb83bc08f3d0f165cd6c95858
LeProblemeEDMN <72100759+LeProblemeEDMN@users.noreply.github.com>
2024-04-09 08:59:57 -0400

    stage v2

--- CSCI/src/Stage.java
+++ CSCI/src/Stage.java
@@ -19,28 +19,41 @@ public class Stage {
         int[][]dynB=new int[l1+1][l2+1];
         int[][]lastHeightB=new int[l1+1][l2+1];
         ///init
+        //refaire init
         lastHeightA[0][0]=a1[0];
         for (int i = 1; i < l1+1; i++) {
             lastHeightA[i][0]=a1[i-1];
+            lastHeightB[i][0]=a1[i-1];
             dynA[i][0]=dynA[i-1][0]+Math.abs(a1[i-1]-lastHeightA[i-1][0]);
+            dynB[i][0]=dynA[i][0];
         }
         lastHeightB[0][0]=a2[0];
         for (int i = 1; i < l2+1; i++) {
             lastHeightB[0][i]=a2[i-1];
+            lastHeightA[0][i]=a2[i-1];
             dynB[0][i]=dynB[0][i-1]+Math.abs(a2[i-1]-lastHeightB[0][i-1]);
+            dynA[0][i]=dynB[0][i];
         }
-        //i pr les i prmeir de A1 j pr les j premier de A2
+       /* for (int i = 1; i < l2+1; i++) {
+            lastHeightA[0][i]=a2[0];
+
+        }
+        for (int i = 1; i < l1+1; i++) {
+            lastHeightB[i][0]=a1[0];
+
+        }*/
+        //i pr les i premier de A1 j pr les j premier de A2
         for (int i = 1; i < l1+1; i++) {
             for (int j = 1; j < l2+1; j++) {
                 int c1=dynA[i-1][j]+Math.abs(a1[i-1]-lastHeightA[i-1][j]);
-                int c2=dynB[i-1][j]+Math.abs(a1[i-1]-lastHeightB[i-1][j]);
+                int c2=dynB[i-1][j]+Math.abs(a1[i-1]-a2[j-1]);
                 if(c1>c2){
                     c1=c2;
                 }
                 dynA[i][j]=c1;
                 lastHeightA[i][j]=a1[i-1];
 
-                c1=dynA[i][j-1]+Math.abs(a2[j-1]-lastHeightA[i][j-1]);
+                c1=dynA[i][j-1]+Math.abs(a2[j-1]-a1[i-1]);
                 c2=dynB[i][j-1]+Math.abs(a2[j-1]-lastHeightB[i][j-1]);
                 if(c1>c2){
                     c1=c2;
